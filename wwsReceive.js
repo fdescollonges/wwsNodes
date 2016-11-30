92d294ef465ce99802f7c307cafda3d39074a3eb
franck <franck@IBM839-PF00TQFM>
2016-11-30 22:49:50 +0100

    First version with Receive

--- wwsReceive.js
+++ wwsReceive.js
@@ -3,73 +3,89 @@
  */
 
 "use strict";
-var request = require('request');
-var url = require('url');
+const request = require('request');
+const url = require('url');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const bparser = require('body-parser');
+const crypto = require('crypto');
+const privateKey  = fs.readFileSync('node_modules/node-red-contrib-wwsNodes/ssl/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('node_modules/node-red-contrib-wwsNodes/ssl/cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate, rejectUnauthorized: false };
 
 module.exports = function(RED) {
-	function wwsRetreive(config) {
+	function wwsReceive(config) {
 		RED.nodes.createNode(this, config);
 		this.wwsApplications = RED.nodes.getNode(config.wwsApplications);
+		console.log("In wwsReceive");
+		var appId = this.wwsApplications.appId;
+		var appSecret = this.wwsApplications.appSecret;
+		var jwtToken = this.wwsApplications.accessToken;
+		var callbackUrl = config.callbackUrl;
+		var whId = config.whId;
+		var whSecret = config.whSecret;
+		console.log("config.whSecret:"+whSecret);
+		var node = this;
 
-		this.on('input', function(msg) {
-			// console.log("In wwsRetreive");
-			var appId = this.wwsApplications.appId;
-			var appSecret = this.wwsApplications.appSecret;
-			var jwtToken = this.wwsApplications.accessToken;
-			var listSpaces = config.listSpaces;
-			var spaceId = config.spaceId;
-			var allSpaces = config.allSpaces;
-			var node = this;
-			
-			// msg.payload = "jwt :" + appId + "/" + appSecret + " Bearer {"+
-			// jwtToken + "}";
-			if (allSpaces) {
-				console.log("Sending to all Spaces");
-				try {
-					var objListSpaces = JSON.parse(listSpaces);
-					for(var i = 0; i < objListSpaces.length; i++) {
-					    var space = objListSpaces[i];
-						console.log("Sending to : "+ space.title +"{"+space.id+"}");
-						sendMessage(msg, space.id, jwtToken, (err, body) => {
-							if (err) {
-								node.error("Unable to send the message : "+msg.payload+" (" + err + ")");
-								node.status({fill:"red",shape:"dot",text:"Not sent"});
-								// console.log (`Unable to send message :
-								// ${err}`);
-							} else { 
-								node.status({fill:"green",shape:"dot",text:"Msg sent"});
-								// console.log (`Message sent : {body}`);
-							};
-						});
-						
-					}
-				} catch (err) {
-					node.error("Unable to send the message : "+msg.payload+" (No spaces)");
-					node.status({fill:"red",shape:"dot",text:"Not sent"});
-				}
-				} else {
-				console.log("Sending to one space : " + spaceId);
-				if (!spaceId) {
-					node.error("Unable to send the message : "+msg.payload+" ( Invalid space )");
-					node.status({fill:"red",shape:"dot",text:"Not sent"});					
-				} else {
-					sendMessage(msg, spaceId, jwtToken, (err, body) => {
-						if (err) {
-							node.error("Unable to send the message : "+msg.payload+" (" + err + ")");
-							node.status({fill:"red",shape:"dot",text:"Not sent"});
-							// console.log (`Unable to send message : ${err}`);
-						} else {
-							node.status({fill:"green",shape:"dot",text:"Msg sent"});
-							// console.log (`Message sent : ${body}`);
-						};
-					});
-				}
-			}
+		var app = express();
+		app.use(rawBody);
 
+		// your express configuration here
+
+		var httpServer = http.createServer(app);
+		var httpsServer = https.createServer(credentials, app);
+		
+		app.get('/wws/wh/callback', function(req, res){
+		    console.log('GET /')
+		    var html = '<html><body><form method="post" action="https://vfea.i234.me:8443/wws/wh/callback">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+		    //var html = fs.readFileSync('index.html');
+		    res.writeHead(200, {'Content-Type': 'text/html'});
+		    res.end(html);
 		});
+
+		app.post('/wws/wh/callback', function(req, res){
+			
+			console.log("In Callback");
+			console.log("Req.header:"+req.headers);
+			console.log("Req.rawBody:"+req.rawBody);
+			console.log("whSecret:"+whSecret);
+			console.log("req.get('X-OUTBOUND-TOKEN'):"+req.get('X-OUTBOUND-TOKEN'));
+			
+			  if (!verifySender(req.headers, req.rawBody, whSecret, req.get('X-OUTBOUND-TOKEN'))) {
+			      console.log("ERROR: Cannot verify caller! -------------");
+			      console.log(req.rawBody.toString());
+			      res.status(200).end();
+			      return;
+			  }
+			  
+			  var body = JSON.parse(req.rawBody.toString());
+			  var eventType = body.type;
+			  if (eventType === "verification") {
+			      handleVerificationRequest(res, body.challenge, whSecret);
+			      console.log("INFO: Verification request processed");
+			      return;
+			  }
+
+			  // Acknowledge we received and processed notification to avoid getting sent the same event again
+			  res.status(200).end();
+
+
+			  if (body.userId === appId) {
+			    console.log("INFO: Skipping our own message Body: " + JSON.stringify(body));
+			    return;
+			  }
+			  var msg = { payload:body };
+			  node.send(msg);
+			  
+		});
+
+		httpServer.listen(8080);
+		httpsServer.listen(8443);
 	}
 	
-	RED.nodes.registerType("wwsRetreive", wwsRetreive);
+	RED.nodes.registerType("wwsReceive", wwsReceive);
 	
 	function sendMessage(msg, spaceId, jwtToken, callback) {
 		var url = `https://api.watsonwork.ibm.com/v1/spaces/${spaceId}/messages`;
@@ -116,44 +132,107 @@ module.exports = function(RED) {
 	}
 	
 
-	const fs = require('fs');
-	const http = require('http');
-	const https = require('https');
-	const express = require('express');
+
+
 	
-	const privateKey  = fs.readFileSync('node_modules/node-red-contrib-wwsNodes/ssl/privkey.pem', 'utf8');
-	const certificate = fs.readFileSync('node_modules/node-red-contrib-wwsNodes/ssl/cert.pem', 'utf8');
+ /*   
+ // Verify Watson Work request signature
+    export const verify = (wsecret) => (req, res, buf, encoding) => {
+      if(req.get('X-OUTBOUND-TOKEN') !==
+        crypto.createHmac('sha256', wsecret).update(buf).digest('hex')) {
+        log('Invalid request signature');
+        const err = new Error('Invalid request signature');
+        err.status = 401;
+        throw err;
+      }
+    };
 
-	const credentials = {key: privateKey, cert: certificate, rejectUnauthorized: false };
+    // Handle Watson Work Webhook challenge requests
+    export const challenge = (wsecret) => (req, res, next) => {
+      if(req.body.type === 'verification') {
+        log('Got Webhook verification challenge %o', req.body);
+        const body = JSON.stringify({
+          response: req.body.challenge
+        });
+        res.set('X-OUTBOUND-TOKEN',
+          createHmac('sha256', wsecret).update(body).digest('hex'));
+        res.type('json').send(body);
+        return;
+      }
+      next();
+    };
 
+    // Create Express Web app
+    export const webapp = (appId, secret, wsecret, cb) => {
+      // Authenticate the app and get an OAuth token
+      oauth.run(appId, secret, (err, token) => {
+        if(err) {
+          cb(err);
+          return;
+        }
 
-	var app = express();
+        // Return the Express Web app
+        cb(null, express()
 
-	// your express configuration here
+          // Configure Express route for the app Webhook
+          .post('/echo',
 
-	var httpServer = http.createServer(app);
-	var httpsServer = https.createServer(credentials, app);
+            // Verify Watson Work request signature and parse request body
+            bparser.json({
+            
+            */
+ //             type: '*/*',
+/*              verify: verify(wsecret)
+            }),
+
+            // Handle Watson Work Webhook challenge requests
+            challenge(wsecret),
+
+            // Handle Watson Work messages
+            echo(appId, token)));
+      });
+    };
+    */
+	function verifySender(headers, rawbody, whSecret, headerToken) {
+	    var expectedToken = crypto
+	        .createHmac("sha256", whSecret)
+	        .update(rawbody)
+	        .digest("hex");
+
+	    if (expectedToken === headerToken) {
+	        return Boolean(true);
+	    } else {
+	        return Boolean(false);
+	    }
+	}
+
+	function handleVerificationRequest(response, challenge, whSecret) {
+	    var responseBodyObject = {
+	        "response": challenge
+	    };
+	    var responseBodyString = JSON.stringify(responseBodyObject);
+
+	    var responseToken = crypto
+	        .createHmac("sha256", whSecret)
+	        .update(responseBodyString)
+	        .digest("hex");
+
+	    response.writeHead(200, {
+	        "Content-Type": "application/json; charset=utf-8",
+	        "X-OUTBOUND-TOKEN": responseToken
+	    });
+
+	    response.end(responseBodyString);
+	}
 	
-	app.get('/wws/wh/callback', function(req, res){
-	    console.log('GET /')
-	    //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-	    var html = fs.readFileSync('index.html');
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end(html);
-	});
-
-	app.post('/wws/wh/callback', function(req, res){
-	    console.log('POST /');
-	    console.log(req.query);
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end('thanks');
-	});
-
-	httpServer.listen(8080);
-	httpsServer.listen(8443);
-	
-    RED.httpAdmin.post('/wws/webhook/callback', function(req, res){
-    	console.log("In webhook callback "+JSON.stringify(req.query));
-    	res.json(req.query);
-    });
-};
+	function rawBody(req, res, next) {
+	    var buffers = [];
+	    req.on("data", function(chunk) {
+	        buffers.push(chunk);
+	    });
+	    req.on("end", function() {
+	        req.rawBody = Buffer.concat(buffers);
+	        next();
+	    });
+	}
+}
