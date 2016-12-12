cb07c44b3c3982c8bcd9e3ddb97c83e15d4428ad
Franck Descollonges <franck_descollonges@fr.ibm.com>
2016-12-12 17:36:11 +0100

    First wwsIn attempt

--- wwsIn.js
+++ wwsIn.js
@@ -25,6 +25,7 @@ module.exports = function(RED) {
     var onHeaders = require('on-headers');
     var typer = require('media-typer');
     var isUtf8 = require('is-utf8');
+    var crypto = require('crypto');
 
     function rawBodyParser(req, res, next) {
         if (req.skipRawBodyParser) { next(); } // don't parse this if told to skip
@@ -166,16 +167,61 @@ module.exports = function(RED) {
         corsHandler = cors(RED.settings.httpNodeCors);
         RED.httpNode.options("*",corsHandler);
     }
+    
+	function verifySender(headers, rawbody, whSecret, headerToken) {
+	    var expectedToken = crypto
+	        .createHmac("sha256", whSecret)
+	        .update(rawbody)
+	        .digest("hex");
+
+	    if (expectedToken === headerToken) {
+	        return Boolean(true);
+	    } else {
+	        return Boolean(false);
+	    }
+	}
+
+	function handleVerificationRequest(response, challenge, whSecret) {
+	    var responseBodyObject = {
+	        "response": challenge
+	    };
+	    var responseBodyString = JSON.stringify(responseBodyObject);
+
+	    var responseToken = crypto
+	        .createHmac("sha256", whSecret)
+	        .update(responseBodyString)
+	        .digest("hex");
+
+	    response.writeHead(200, {
+	        "Content-Type": "application/json; charset=utf-8",
+	        "X-OUTBOUND-TOKEN": responseToken
+	    });
+
+	    response.end(responseBodyString);
+	}
+	
+	function rawBody(req, res, next) {
+	    var buffers = [];
+	    req.on("data", function(chunk) {
+	        buffers.push(chunk);
+	    });
+	    req.on("end", function() {
+	        req.rawBody = Buffer.concat(buffers);
+	        next();
+	    });
+	}
 
     function wwsIn(n) {
         RED.nodes.createNode(this,n);
         if (RED.settings.httpNodeRoot !== false) {
 
-            if (!n.url) {
+            if (!n.callbackUrl) {
                 this.warn(RED._("httpin.errors.missing-path"));
                 return;
             }
             this.url = n.callbackUrl;
+            this.whSecret = n.whSecret;
+            this.appId = n.appId;
             //this.method = n.method;
             //this.swaggerDoc = n.swaggerDoc;
 
@@ -189,10 +235,53 @@ module.exports = function(RED) {
             this.callback = function(req,res) {
                 var msgid = RED.util.generateId();
                 res._msgid = msgid;
-                if (req.method=="get") {
-                    node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.query});
-                } else {
-                    node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.body});
+    		    console.log('Message received : '+JSON.stringify(req.body));
+                if (req.method=="GET") {
+        		    console.log('GET /');
+        		    //var html = '<html><body><form method="post" action="https://vfea.i234.me:8443/wws/wh/callback">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        		    //var html = fs.readFileSync('index.html');
+        		    var html;
+        		    if (req.secure) {
+        		    	html = '<html><body>Server running on : https://'+req.headers.host+req.url+'</form></body>';
+        		    } else {
+        		    	html = '<html><body>Server running on : http://'+req.headers.host+req.url+'</form></body>';		    	
+        		    }
+        		    res.writeHead(200, {'Content-Type': 'text/html'});
+        		    res.end(html);
+                    //node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.query});
+                };
+                if (req.method=="POST") {
+        		    console.log('POST /');
+        			console.log("In Callback");
+        			console.log("Req.header:"+req.headers);
+        			console.log("Req.rawBody:"+req.rawBody);
+        			console.log("whSecret:"+node.whSecret);
+        			console.log("req.get('X-OUTBOUND-TOKEN'):"+req.get('X-OUTBOUND-TOKEN'));
+        			
+        			if (!verifySender(req.headers, req.rawBody, node.whSecret, req.get('X-OUTBOUND-TOKEN'))) {
+        				console.log("ERROR: Cannot verify caller! -------------");
+        			    console.log(req.rawBody.toString());
+        			    res.status(200).end();
+        			    return;
+        			}
+        			var body = JSON.parse(req.rawBody.toString());
+        			var eventType = body.type;
+        			if (eventType === "verification") {
+        			    handleVerificationRequest(res, body.challenge, node.whSecret);
+        			    console.log("INFO: Verification request processed");
+        			    return;
+        			}
+                    
+        			// Acknowledge we received and processed notification to avoid getting sent the same event again
+        			res.status(200).end();
+        			if (body.userId === node.appId) {
+        				console.log("INFO: Skipping our own message Body: " + JSON.stringify(body));
+        			    return;
+        			}
+        			// passing the message to next Node
+        			var msg = { payload:body };
+        			node.send(msg);  
+                    //node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.query});
                 }
             };
 
@@ -223,20 +312,23 @@ module.exports = function(RED) {
                 };
             }
 
-            RED.httpNode.get(this.url,cookieParser(),httpMiddleware,corsHandler,metricsHandler,this.callback,this.errorHandler);
-            RED.httpNode.post(this.url,cookieParser(),httpMiddleware,corsHandler,metricsHandler,jsonParser,urlencParser,rawBodyParser,this.callback,this.errorHandler);
+            //RED.httpNode.all(this.url,cookieParser(),httpMiddleware,corsHandler,metricsHandler,this.callback,this.errorHandler);
+            RED.httpNode.all(this.url,cookieParser(),httpMiddleware,corsHandler,metricsHandler,jsonParser,urlencParser,rawBody,this.callback,this.errorHandler);
 
             this.on("close",function() {
                 var node = this;
                 RED.httpNode._router.stack.forEach(function(route,i,routes) {
-                    if (route.route && route.route.path === node.url && route.route.methods[node.method]) {
+                    if (route.route && route.route.path === node.url) {
                         routes.splice(i,1);
                     }
+                    //if (route.route && route.route.path === node.url && route.route.methods["post"]) {
+                    //    routes.splice(i,1);
+                    //}
                 });
             });
         } else {
             this.warn(RED._("httpin.errors.not-created"));
         }
     }
-    RED.nodes.registerType("wwsI",wwsIn);
+    RED.nodes.registerType("wwsIn",wwsIn);
 }
